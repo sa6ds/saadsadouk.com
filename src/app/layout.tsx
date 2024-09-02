25eb8e39c188746176c25e7de92926ed07816f35
s <96222718+sa6ds@users.noreply.github.com>
2024-09-01 23:52:05 -0400

    create og-image & update metadata for pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,35 @@ import { themeEffect } from "./components/theme/themeEffect";
 
 export const metadata: Metadata = {
   title: "Saad Sadouk - Software Engineer",
-  description: "Full-Stack Developer and student at George Mason University",
+  description:
+    "Full-Stack Developer and student at George Mason University. Explore my projects, skills, and experiences.",
+  metadataBase: new URL("https://www.saadsadouk.com"),
+  openGraph: {
+    title: "Saad Sadouk - Software Engineer",
+    description:
+      "Full-Stack Developer and student at George Mason University. Explore my projects, skills, and experiences.",
+    url: "https://www.saadsadouk.com",
+    siteName: "Saad Sadouk",
+    images: [
+      {
+        url: "https://www.saadsadouk.com/api/og",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Saad Sadouk - Software Engineer",
+    description:
+      "Full-Stack Developer and student at George Mason University. Explore my projects, skills, and experiences.",
+    images: ["https://www.saadsadouk.com/api/og"],
+  },
+  alternates: {
+    canonical: "https://www.saadsadouk.com",
+  },
   icons: [
     {
       media: "(prefers-color-scheme: light)",
@@ -27,6 +55,27 @@ export default function RootLayout({
   return (
     <html lang="en" className={`antialiased`} suppressHydrationWarning={true}>
       <head>
+        <link
+          rel="icon"
+          href="/svgs/faviconLight.svg"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          href="/svgs/faviconDark.svg"
+          media="(prefers-color-scheme: dark)"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/svgs/faviconLight.svg"
+        />
+
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/svgs/faviconLight.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <script
           dangerouslySetInnerHTML={{
             __html: `(${themeEffect.toString()})();`,
